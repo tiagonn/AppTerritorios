ac235d3a47606bdefa61becf95e4f963b071ecdc
Tiago Nascimento <tiagonn@gmail.com>
2015-04-20 15:17:07 -0300

    CR 51 - Revisar layout de sistema - em andamento

--- ApoioTerritorioLS/src/br/com/nascisoft/apoioterritoriols/resources/client/CellTableCustomResources.java
+++ ApoioTerritorioLS/src/br/com/nascisoft/apoioterritoriols/resources/client/CellTableCustomResources.java
@@ -5,12 +5,18 @@ import com.google.gwt.user.cellview.client.CellTable.Resources;
 import com.google.gwt.user.cellview.client.CellTable.Style;
 
 public interface CellTableCustomResources extends Resources {
-	
-	public static final CellTableCustomResources INSTANCE = GWT.create(CellTableCustomResources.class);
-	
-    @Source({Style.DEFAULT_CSS, "/css/CustomCellTable.css"})
-    Style cellTableStyle();
-    
-    public interface CustomCellTableStyle extends Style {
+
+	public static final CellTableCustomResources INSTANCE = GWT
+			.create(CellTableCustomResources.class);
+
+	@Override
+	@Source({ Style.DEFAULT_CSS, "/css/CustomCellTable.css" })
+	TableStyle cellTableStyle();
+
+	/**
+	 * The styles applied to the table.
+	 */
+	interface TableStyle extends Style {
 	}
+
 }

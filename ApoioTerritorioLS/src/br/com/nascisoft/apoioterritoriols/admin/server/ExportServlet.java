cb9dc869ce92c7135418e4b4c398a7847fd992f9
tiago <tiago@192.168.0.12>
2016-09-08 22:54:55 -0300

    Não relançando exception de geração de export

--- ApoioTerritorioLS/src/br/com/nascisoft/apoioterritoriols/admin/server/ExportServlet.java
+++ ApoioTerritorioLS/src/br/com/nascisoft/apoioterritoriols/admin/server/ExportServlet.java
@@ -42,99 +42,104 @@ public class ExportServlet extends AbstractApoioTerritorioLSHttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		
-		String destinatarios = req.getParameter("destinatario");
+		try {
 		
-		String remetente = System.getProperty("email.admin");
-		
-		if (!StringUtils.isEmpty(destinatarios)) {
+			String destinatarios = req.getParameter("destinatario");
 			
-			RelatorioVO relatorio = (new AdminServiceImpl()).obterDadosRelatorio();
-			Set<String> chaves = relatorio.keySet();
-			List<String> chavesOrdenadas = new ArrayList<String>();
-			chavesOrdenadas.addAll(chaves);
-			Collections.sort(chavesOrdenadas);
+			String remetente = System.getProperty("email.admin");
 			
-			StringBuffer csvRegiao = new StringBuffer("Cidade;Região;QuantidadePessoas;QuantidadeEnderecos\n");
-			StringBuffer csvBairro = new StringBuffer("Cidade;Região;Bairro;QuantidadePessoas;QuantidadeEnderecos\n");
-			String bairroNaoInformado = "Não informado";
-
-			for (String regiao : chavesOrdenadas) {
-				csvRegiao
-					.append(relatorio.get(regiao).getCidade()).append(";")
-					.append(regiao).append(";")
-					.append(relatorio.get(regiao).getConsolidadoPessoas()).append(";")
-					.append(relatorio.get(regiao).getConsolidadoEnderecos()).append("\n");
-				Set<String> chavesBairros = relatorio.get(regiao).getMapaBairros().keySet();
-				List<String> chavesBairrosOrdenados = new ArrayList<String>();
-				chavesBairrosOrdenados.addAll(chavesBairros);
-				Collections.sort(chavesBairrosOrdenados);
-				for (String bairro : chavesBairrosOrdenados) {
-					if(!bairroNaoInformado.equals(bairro)) {
-						String[] quantidades = relatorio.get(regiao).getMapaBairros().get(bairro).split("/");
+			if (!StringUtils.isEmpty(destinatarios)) {
+				
+				RelatorioVO relatorio = (new AdminServiceImpl()).obterDadosRelatorio();
+				Set<String> chaves = relatorio.keySet();
+				List<String> chavesOrdenadas = new ArrayList<String>();
+				chavesOrdenadas.addAll(chaves);
+				Collections.sort(chavesOrdenadas);
+				
+				StringBuffer csvRegiao = new StringBuffer("Cidade;Região;QuantidadePessoas;QuantidadeEnderecos\n");
+				StringBuffer csvBairro = new StringBuffer("Cidade;Região;Bairro;QuantidadePessoas;QuantidadeEnderecos\n");
+				String bairroNaoInformado = "Não informado";
+	
+				for (String regiao : chavesOrdenadas) {
+					csvRegiao
+						.append(relatorio.get(regiao).getCidade()).append(";")
+						.append(regiao).append(";")
+						.append(relatorio.get(regiao).getConsolidadoPessoas()).append(";")
+						.append(relatorio.get(regiao).getConsolidadoEnderecos()).append("\n");
+					Set<String> chavesBairros = relatorio.get(regiao).getMapaBairros().keySet();
+					List<String> chavesBairrosOrdenados = new ArrayList<String>();
+					chavesBairrosOrdenados.addAll(chavesBairros);
+					Collections.sort(chavesBairrosOrdenados);
+					for (String bairro : chavesBairrosOrdenados) {
+						if(!bairroNaoInformado.equals(bairro)) {
+							String[] quantidades = relatorio.get(regiao).getMapaBairros().get(bairro).split("/");
+							csvBairro.append(relatorio.get(regiao).getCidade()).append(";")
+								.append(regiao).append(";")
+								.append(bairro).append(";")
+								.append(quantidades[0]).append(";")
+								.append(quantidades[1]).append("\n");
+						}
+					}
+					if (relatorio.get(regiao).getMapaBairros().get(bairroNaoInformado) != null) {
+						String[] quantidades = relatorio.get(regiao).getMapaBairros().get(bairroNaoInformado).split("/");
 						csvBairro.append(relatorio.get(regiao).getCidade()).append(";")
 							.append(regiao).append(";")
-							.append(bairro).append(";")
+							.append(bairroNaoInformado).append(";")
 							.append(quantidades[0]).append(";")
 							.append(quantidades[1]).append("\n");
 					}
 				}
-				if (relatorio.get(regiao).getMapaBairros().get(bairroNaoInformado) != null) {
-					String[] quantidades = relatorio.get(regiao).getMapaBairros().get(bairroNaoInformado).split("/");
-					csvBairro.append(relatorio.get(regiao).getCidade()).append(";")
-						.append(regiao).append(";")
-						.append(bairroNaoInformado).append(";")
-						.append(quantidades[0]).append(";")
-						.append(quantidades[1]).append("\n");
+	
+				ByteArrayOutputStream outRegiao = new ByteArrayOutputStream();
+				outRegiao.write(csvRegiao.toString().getBytes("ISO-8859-1"));
+				ByteArrayOutputStream outBairro = new ByteArrayOutputStream();
+				outRegiao.write(csvBairro.toString().getBytes("ISO-8859-1"));
+				
+				ByteArrayOutputStream zipOut = new ByteArrayOutputStream();
+				ZipOutputStream zip = new ZipOutputStream(zipOut);
+				
+				ZipEntry entryRegiao = new ZipEntry("exportApoioTerritorio.csv");
+				zip.putNextEntry(entryRegiao);
+				zip.write(outRegiao.toByteArray());
+				zip.write(outBairro.toByteArray());
+				
+				zip.close();
+				
+				DataSource ds = new ByteArrayDataSource(zipOut.toByteArray(), "aplication/zip");
+										
+				Properties props = new Properties();
+		        Session session = Session.getDefaultInstance(props, null);
+	
+		        try {
+					Message msg = new MimeMessage(session);
+					msg.setFrom(new InternetAddress(remetente));
+					msg.addRecipient(Message.RecipientType.TO,
+					                 new InternetAddress(destinatarios));
+					msg.setSubject("Export de relatorio de enderecos/pessoas do APPTerritorios");
+					msg.setText("Segue em anexo o export. \n\nNote que por limitação de segurança do google o anexo é um arquivo .zipe. " +
+							"Por favor renomeie o arquivo para .zip e depois abra normalmente.");
+					Multipart mp = new MimeMultipart();						
+					 
+					MimeBodyPart htmlPart = new MimeBodyPart();
+				    htmlPart.setContent("Segue em anexo o export. <br/><br/>Note que por limitação de segurança do google o anexo é um arquivo .zipe. " +
+							"Por favor renomeie o arquivo para .zip e depois abra normalmente.", "text/html");
+				    mp.addBodyPart(htmlPart);
+				    
+					MimeBodyPart attachment = new MimeBodyPart();
+					attachment.setFileName("exportApoioTerritorioLS.zipe");
+					attachment.setDataHandler(new DataHandler(ds));
+					mp.addBodyPart(attachment);
+					
+					msg.setContent(mp);
+					
+					Transport.send(msg);
+		        } catch (Exception e) {
+					logger.log(Level.SEVERE, "Erro ao enviar e-mail de export", e);
+					throw new ServletException(e);
 				}
 			}
-
-			ByteArrayOutputStream outRegiao = new ByteArrayOutputStream();
-			outRegiao.write(csvRegiao.toString().getBytes("ISO-8859-1"));
-			ByteArrayOutputStream outBairro = new ByteArrayOutputStream();
-			outRegiao.write(csvBairro.toString().getBytes("ISO-8859-1"));
-			
-			ByteArrayOutputStream zipOut = new ByteArrayOutputStream();
-			ZipOutputStream zip = new ZipOutputStream(zipOut);
-			
-			ZipEntry entryRegiao = new ZipEntry("exportApoioTerritorio.csv");
-			zip.putNextEntry(entryRegiao);
-			zip.write(outRegiao.toByteArray());
-			zip.write(outBairro.toByteArray());
-			
-			zip.close();
-			
-			DataSource ds = new ByteArrayDataSource(zipOut.toByteArray(), "aplication/zip");
-									
-			Properties props = new Properties();
-	        Session session = Session.getDefaultInstance(props, null);
-
-	        try {
-				Message msg = new MimeMessage(session);
-				msg.setFrom(new InternetAddress(remetente));
-				msg.addRecipient(Message.RecipientType.TO,
-				                 new InternetAddress(destinatarios));
-				msg.setSubject("Export de relatorio de enderecos/pessoas do APPTerritorios");
-				msg.setText("Segue em anexo o export. \n\nNote que por limitação de segurança do google o anexo é um arquivo .zipe. " +
-						"Por favor renomeie o arquivo para .zip e depois abra normalmente.");
-				Multipart mp = new MimeMultipart();						
-				 
-				MimeBodyPart htmlPart = new MimeBodyPart();
-			    htmlPart.setContent("Segue em anexo o export. <br/><br/>Note que por limitação de segurança do google o anexo é um arquivo .zipe. " +
-						"Por favor renomeie o arquivo para .zip e depois abra normalmente.", "text/html");
-			    mp.addBodyPart(htmlPart);
-			    
-				MimeBodyPart attachment = new MimeBodyPart();
-				attachment.setFileName("exportApoioTerritorioLS.zipe");
-				attachment.setDataHandler(new DataHandler(ds));
-				mp.addBodyPart(attachment);
-				
-				msg.setContent(mp);
-				
-				Transport.send(msg);
-	        } catch (Exception e) {
-				logger.log(Level.SEVERE, "Erro ao enviar e-mail de export", e);
-				throw new ServletException(e);
-			}
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "Erro ao preparar backup ou ao enviar e-mail de export. Não relançando a exception para a mensagem não voltar para a fila.", e);
 		}
 	}
 }
